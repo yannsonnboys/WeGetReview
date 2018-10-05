f210a8e4843969a7dc7915a820019d1e7d3b831c
yannsonnboys <yannsonnboys01@gmail.com>
2018-10-05 19:25:02 +0200

    Update Add images with carrierwave, S3, and Figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["aws_access_key_id"],                        # required unless using use_iam_profile
     aws_secret_access_key: ENV["aws_secret_access_key"],                        # required unless using use_iam_profile
+    region:                ENV["region"],  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = ENV["fog_directory"]                                     # required
 end
