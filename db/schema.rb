8ca90c30844b1f81804ab1a353a0ba705cd9b2c1
yannsonnboys <yannsonnboys01@gmail.com>
2018-10-15 22:54:14 +0200

    Add Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_200211) do
+ActiveRecord::Schema.define(version: 2018_10_15_203655) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2018_10_09_200211) do
     t.string "address"
     t.string "phone"
     t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
